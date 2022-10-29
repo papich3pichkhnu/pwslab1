43f64498d1ba33db1b24a8e51f18101d7673ed62
papich3pich <kovalleonid4@gmail.com>
2022-10-29 15:47:14 +0300

    changed namespaces

--- lab1_pws/FileLogger/FileLoggerExtensions.cs
+++ lab1_pws/FileLogger/FileLoggerExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab1_pws.Services.Services
+namespace lab1_pws
 {
     public static class FileLoggerExtensions
     {
