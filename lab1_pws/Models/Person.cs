2bfa0b58081a54469643d2e2cb45767cd814a451
papich3pich <kovalleonid4@gmail.com>
2022-11-12 15:16:17 +0200

    added PersonForm validator

---
+++ lab1_pws/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace lab1_pws.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        
+    }
+   
+}
+
---
+++ lab1_pws/Validators/PersonValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using lab1_pws.Models;
+using System;
+using System.Linq;
+
+namespace lab1_pws.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).Length(0, 10).NotNull();
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Age).InclusiveBetween(18, 60);
+            RuleFor(x => x.Job).Length(4, 20);
+           
+        }
+        
+    }
+}
